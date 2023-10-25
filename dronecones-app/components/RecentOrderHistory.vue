0858e40f631ed7d067607801271a5edf04a12b4d
Samuel McMillan <a02261822@usu.edu>
2023-10-25 11:22:43 -0600

    RecentOrderHistory almost ready for deployment

--- dronecones-app/components/RecentOrderHistory.vue
+++ dronecones-app/components/RecentOrderHistory.vue
@@ -1,5 +1,6 @@
 <script>
-    import { supabase } from '../components/lib/supabaseClient'
+    import { getUserOrderHistory } from '../database/transactionDatabase'
+    import { supabase } from './lib/supabaseClient'
 
     export default {
         data() {
@@ -9,13 +10,8 @@
         },
         async created() {
             try {
-                const { data, error } = await supabase
-                    .from('transactions')
-                    .select()
-                    .eq('userID', supabase.auth.user().id)
-                    .order('orderTime', { ascending: false })
-                    .limit(3);
-
+                const { data, error } = 
+                    getUserOrderHistory(userId = supabase.auth.user().id, limit = 3);
                 if (error) {
                     console.error('Error fetching transactions:', error);
                 } else {
@@ -25,12 +21,7 @@
                 console.error('An error occurred:', error);
             }
         },
-        methods: {
-            reorderOrder(order) {
-                // Implement logic to create a new order with the items from the selected past order.
-                // You can display a confirmation or navigate to the order page.
-            },
-        },
+
     };
 </script>
 
@@ -38,13 +29,10 @@
     <div>
         <h2>Ice Cream Order History</h2>
         <ul>
-            <li v-for="order in pastOrders" :key="order.id">
+            <li v-for="order in pastOrders">
                 <div>
                     <!-- Display the past order details -->
-                    <p>Order Time: {{ order.orderTime }}</p>
-                    <p v-for="item in order.items">
-                        {{ item.quantity }} x {{ item.name }}
-                    </p>
+                    {{ order.cone }}
                 </div>
                 <button @click="reorderOrder(order)">Reorder</button>
             </li>
