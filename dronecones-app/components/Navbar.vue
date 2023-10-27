9593f5e1ef4c9a95de3a6e564b8e256ed25dc226
Jordan Olsen <a02325079@usu.edu>
2023-10-26 18:20:50 -0600

    started making menu

--- dronecones-app/components/Navbar.vue
+++ dronecones-app/components/Navbar.vue
@@ -1,7 +1,15 @@
+<script>
+    document.getElementById("menu-button").addEventListener("click", ()=> {
+        document.getElementById("menu-bar").classList.toggle("open");
+    });
+</script>
+
+
+
 <template>
     <div class = "navbar-under"></div>
     <nav class = "navbar">
-        <button class = "menu-button">Menu</button>
+        <button class = "menu-button" id = "menu-button">Menu</button>
         <div class = "title">
             Drone Cones
         </div>
@@ -11,4 +19,9 @@
             <a href="/">Signout</a>
         </div>
     </nav>
+    <div class = "menu-bar" id = "menu-bar">
+        <a href="order">Order</a>
+        <a href="droneHome">Drones</a>
+        <a href="/">Signout</a>
+    </div>
 </template>
