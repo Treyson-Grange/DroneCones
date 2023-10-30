bd5d4d704da8ec66aad14834e8e8a14aa60b8bb3
Jordan Olsen <a02325079@usu.edu>
2023-10-30 10:22:45 -0600

    Created drone items and main header along with the add drone button

--- dronecones-app/components/Drones.vue
+++ dronecones-app/components/Drones.vue
@@ -3,7 +3,26 @@
 </script>
 
 <template>
-    <div>
-        <h1>Drones</h1>
+    <div class = "page2">
+        <div class = "drone-header">
+            <h1>Drones</h1>
+            <button>Add Drone</button>
+        </div>
+        <div class = 'drone-item'>
+            <h2>Example Name</h2>
+            <div class = 'drone-info'>
+                <div>Size: medium</div>
+                <div>Status: disabled</div>
+            </div>
+            <form>
+                <label for="status">Choose a Status:</label>
+                <select id="drone-status" name="status">
+                    <option value="enabled">Enabled</option>
+                    <option value="disabled">Disabled</option>
+                    <option value="delete">Delete</option>
+                </select>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
     </div>
 </template>
