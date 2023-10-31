b6b07241a607893cace39d2c4f17df50fcfe90f0
Jordan Olsen <a02325079@usu.edu>
2023-10-31 10:16:23 -0600

    got basic layout for adding a drone

--- dronecones-app/components/AddDrone.vue
+++ dronecones-app/components/AddDrone.vue
@@ -1,5 +1,24 @@
 <template>
     <div class = "page2">
         <h1>Add New Drone</h1>
+        <div class = "login">
+            <form>
+                <div class = "form-group">
+                    <label for ="name">Name:</label>
+                    <input type = "text" id = "name"/>
+                    <div class = "choices">
+                        <button>Small</button>
+                        <button>Medium</button>
+                        <button>Large</button>
+                    </div>
+                    <label for = "radius">Drone Radius:</label>
+                    <input type = "number" id = "radius"/>
+                </div>
+                <div class = "spacer">
+                    <button style = "background-color: var(--accent-color); color: var(--font-accent);">Add</button>
+                    <button>Cancel</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
