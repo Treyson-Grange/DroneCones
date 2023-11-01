70dabe4bca851dc6398bde9dfcb10343d024d359
Samuel McMillan <a02261822@usu.edu>
2023-11-01 09:07:17 -0600

    Manager Account Information Page Framework Created

---
+++ dronecones-app/components/bankInfo.vue
@@ -0,0 +1,35 @@
+<script>
+    // import { bankInfo } from '../database/transactionDatabase'
+    // export default {
+    //     data() {
+    //         return {
+    //             routingNumber: "", 
+    //             accountNumber: "", 
+    //         };
+    //     },
+    //     async created() {
+    //         console.log(routingNumber);
+    //         try {
+        //         const data = await bankInfo(localStorage.getItem('userID'));
+                //data.routingNumber;
+                //data.accountNumber;
+    //         } catch (error) {
+    //             console.error('An error occurred:', error);
+    //         }
+    //     },
+    // };
+</script>
+
+<template>
+    <div>
+        <h2>Bank Information</h2>
+        <ul>
+            <li>
+                routingNumber:
+            </li>
+            <li v>
+                accountNumber:
+            </li>
+        </ul>
+    </div>
+</template>
