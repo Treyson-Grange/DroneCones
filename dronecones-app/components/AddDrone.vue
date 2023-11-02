a78e70c271985e273a98452f6012543d414a0ce5
Jordan Olsen <a02325079@usu.edu>
2023-11-02 17:01:03 -0600

    Adding a drone is now complete and working

--- dronecones-app/components/AddDrone.vue
+++ dronecones-app/components/AddDrone.vue
@@ -1,7 +1,26 @@
 <script setup>
     import { supabase } from './lib/supabaseClient'
+    import {addDrone} from '../database/droneDatabase'
+
+    const formData = {
+        name: '',
+        size: ''
+    }
+
+    async function createDrone(){
+        const sizeValue = Number(formData.size);
+        const error = await addDrone(localStorage.getItem("userID"), formData.name.toString(), sizeValue);
+        if(error){
+            console.log("Creating Drone Error: ", error.message);
+        }
+        else{
+            console.log(formData);
+        }
+    }
+
     function navigateToDrones(){
-        window.location.href = '/droneHome'
+        console.log("hello");
+        window.location.href = "/droneHome";
     }
 </script>
 
@@ -10,19 +29,19 @@
     <div class = "page2">
         <h1>Add New Drone</h1>
         <div class = "form-list">
-            <form>
+            <form @submit.once="createDrone">
                 <div class = "form-group">
                     <label for ="name">Name:</label>
-                    <input type = "text" id = "name"/>
+                    <input type = "text" id = "name" v-model="formData.name"/>
                 </div>
                 <div class="radio-button-container">
-                    <input class = "radio" type="radio" name="name1" id="small">
+                    <input name = "size" class = "radio" type="radio" id="small" value = '0' v-model="formData.size">
                     <label class = "radio-button" for="small">Small</label>
 
-                    <input class = "radio" type="radio" name="name1" id="medium">
+                    <input name = "size" class = "radio" type="radio" id="medium" value = '1' v-model="formData.size">
                     <label class = "radio-button" for="medium">Medium</label>
 
-                    <input class = "radio" type="radio" name="name1" id="large">
+                    <input name = "size" class = "radio" type="radio" id="large" value = '2' v-model="formData.size">
                     <label class = "radio-button" for="large">Large</label>
                 </div>
                 <div class = "spacer">
