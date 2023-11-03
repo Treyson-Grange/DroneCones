b5408aa5fc351a79a40ea526f7627dc1fdc19c56
Jordan Olsen <a02325079@usu.edu>
2023-11-03 11:25:11 -0600

    started support page

---
+++ dronecones-app/pages/support.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import data from '../content/home.json'
+
+    
+</script>
+
+
+
+<template>
+    
+    <Navbar>
+        <!-- make conditional for links -->
+    </Navbar>
+    <h1>Support</h1>
+    <Support/>
+</template>
