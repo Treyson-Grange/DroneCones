03dc8917211311f46f6c47629a204dc440e95a5b
Samuel McMillan <a02261822@usu.edu>
2023-11-04 23:00:29 -0600

    The revenue graphs are totally up and runninggit status

--- dronecones-app/components/revenueGraphs.vue
+++ dronecones-app/components/revenueGraphs.vue
@@ -2,24 +2,28 @@
     <div>
       <h2>Revenue</h2>
       <canvas ref="myChart"></canvas>
+      <canvas ref="myOtherChart"></canvas>
     </div>
   </template>
   
 <script>
 import { Chart } from 'chart.js/auto';
-import { getRevenueHistory } from '../database/transactionDatabase';
+import { getTransactions } from '../database/transactionDatabase';
 
   
 export default {
     data() {
         return {
             chartData: [],
+            currentYearchartData: [],
+            yearsInOperation: [],
+            revenueByYear: [],
         };
     },
 
     async created() {
         try {
-            const data = await getRevenueHistory();
+            const data = await getTransactions();
             this.chartData = data;
         } catch (error) {
             console.error('An error occurred:', error);
@@ -34,30 +38,90 @@ export default {
         const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         
         // filter chart data for current year
-        this.chartData = this.chartData.filter((data) => new Date(data.order_time).getFullYear() == currentDate.getFullYear());
+        this.currentYearchartData = this.chartData.filter((data) => new Date(data.order_time).getFullYear() == currentDate.getFullYear());
         var monthList = Array.from({ length: currentDate.getMonth() + 1 }, (_, i) => i); // .getMonth() starts at 0 for january and ends at 11 for december so we add one to make it the number of months we want
-        var theLabels = monthList.map((month) => monthNames[month]);
+        var theMonthLabels = monthList.map((month) => monthNames[month]);
         var monthRevenues = new Array(monthList.length).fill(0);
-        this.chartData.forEach((data) => {
+        this.currentYearchartData.forEach((data) => {
             var order_time = new Date(data.order_time);
             if (monthList.includes(order_time.getMonth())) {
                 monthRevenues[order_time.getMonth()] += data.final_price;
             }
         });
-        
+
+        this.chartData.forEach((data) => {
+            var order_time = new Date(data.order_time);
+            if (!this.yearsInOperation.includes(order_time.getFullYear())) {
+                this.yearsInOperation.push(order_time.getFullYear());
+                this.revenueByYear.push(data.final_price);
+            } else {
+                this.revenueByYear[this.revenueByYear.length -1] += data.final_price;
+            }
+        });
+
         new Chart(this.$refs.myChart, {
             type: 'bar',
             data: {
                 datasets: [{
                     label: 'Current Year Revenue',
                     data: monthRevenues,
-                    // this dataset is drawn below
-                    order: 1
+                    backgroundColor: '#4cc9f0',
                 }],
-                labels: theLabels
+                labels: theMonthLabels
             },
             options: {
+                color: '#4cc9f0',
                 responsive: true,
+                scales: {
+                    y: {
+                        ticks: {
+                            color: '#4cc9f0'
+                        },
+                        title: {
+                            color: '#4cc9f0',
+                            display: true,
+                            text: 'US dollars'
+                        }
+                    },
+                    x: {
+                        ticks: {
+                            color: '#4cc9f0'
+                        },
+                    },
+                    }
+                },
+            });
+        
+        new Chart(this.$refs.myOtherChart, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: 'Revenue By Year',
+                    data: this.revenueByYear,
+                    backgroundColor: '#4cc9f0',
+                }],
+                labels: this.yearsInOperation
+            },
+            options: {
+                color: '#4cc9f0',
+                responsive: true,
+                scales: {
+                    y: {
+                        ticks: {
+                            color: '#4cc9f0'
+                        },
+                        title: {
+                            color: '#4cc9f0',
+                            display: true,
+                            text: 'US dollars'
+                        }
+                    },
+                    x: {
+                        ticks: {
+                            color: '#4cc9f0'
+                        },
+                    },
+                    }
             },
             });
     },
