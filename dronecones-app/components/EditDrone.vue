61dcf8e6060ff4b8c72dccb70a4cd60c347ea1a4
Jordan Olsen <a02325079@usu.edu>
2023-11-06 09:45:51 -0700

    moved edit drone components to a seperate file

--- dronecones-app/components/EditDrone.vue
+++ dronecones-app/components/EditDrone.vue
@@ -1,5 +1,43 @@
 <template>
-    
+  <form :class="{'show-choices' : showOptions, 'hide-choices': !showOptions}" style = "width:100%;">
+    <div class = "form-group">
+      <label for = "name">Enter new name:</label>
+      <input type = "text" id = "name" v-model="drone.name">
+    </div>
+    <div class = "form-group">
+      <label for="size">Change Status:</label>
+      <div class="radio-button-container" style="margin-top: 0px;" id = "size" :key="index">
+            <input class = "radio" type="radio" name="size1" id="radio-1">
+            <label class = "radio-button" for="radio-1">Small</label>
+
+            <input class = "radio" type="radio" name="size1" id="radio-2">
+            <label class = "radio-button" for="radio-2">Medium</label>
+
+            <input class = "radio" type="radio" name="size1" id="radio-3">
+            <label class = "radio-button" for="radio-3">Large</label>
+        </div>
+    </div>
+    <div class = "form-group">
+      <label for="status">Change Status:</label>
+      <div class="radio-button-container" style="margin-top: 0px;" id = "status">
+            <input class = "radio" type="radio" name="name1" id="radio-1">
+            <label class = "radio-button" for="radio-1">Enable</label>
+
+            <input class = "radio" type="radio" name="name1" id="radio-2">
+            <label class = "radio-button" for="radio-2">Disable</label>
+
+            <input class = "radio" type="radio" name="name1" id="radio-3">
+            <label class = "radio-button" for="radio-3">Delete</label>
+        </div>
+    </div>
+    <div class = "end">
+      <button 
+      style = "background-color: var(--accent-color); color: var(--font-accent);"
+      type="submit">
+        Save
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
