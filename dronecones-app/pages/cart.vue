2e6d16571126c88c12461dbcee474d34f9c35cf9
nathan <nathanctay@gmail.com>
2023-11-10 14:35:32 -0700

    Cart page

---
+++ dronecones-app/pages/cart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<style>
+    @import url('/assets/style.css');
+</style>
+
+<template>
+    <Navbar>
+        <a href="/order">Order</a>
+        <a href="/cart">Cart</a>
+        <a href="/support">Support</a>
+    </Navbar>
+    <Cart />
+</template>
