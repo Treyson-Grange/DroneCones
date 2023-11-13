71f26c856fd307c1e94645e2377e02d9ff0c1ba8
Jordan Olsen <a02325079@usu.edu>
2023-11-12 20:46:03 -0700

    finshed revenues? Still need some values to know for sure

--- dronecones-app/components/Drones.vue
+++ dronecones-app/components/Drones.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { supabase } from './lib/supabaseClient'
-    import {getDrones} from '../database/droneDatabase'
+    import { getDroneDeliveries, getDrones } from '~/database/droneDatabase';
+    import { getTransaction } from '~/database/transactionDatabase'
 </script>
 
 <template>
@@ -31,7 +32,7 @@
                     <span v-else>Enabled</span>
                 </div>
                 <div>
-                  <strong>Revenue: </strong>
+                  <strong>Revenue:</strong> ${{ this.revenues[index] }}
                 </div>
             </div>
         </div>
@@ -44,8 +45,13 @@ export default {
     return {
       showOptions: false,
       drones: [],
+      revenues: [],
       id: localStorage.getItem('userID') || 'Data not set'
     };
+  },
+  async created(){
+    await this.retrieveDrones();
+    this.getTotalRevenue();
   },
   methods: {
     refreshDrones(){
@@ -54,16 +60,37 @@ export default {
                   this.drones = drones
               })
     },
+    async retrieveDrones() {
+      // You can call the getDrones function here or in any method as needed
+      try {
+        const limit = 50; // Specify your desired limit
+        this.drones = await getDrones(limit, this.id);
+        console.log('Drones:', this.drones);
+      } catch (error) {
+        console.error('Error fetching drones:', error);
+      }
+    },
+    async getTotalRevenue() {
+      console.log("hello");
+      try {
+          for(let i = 0; i < this.drones.length; i++) {
+              this.revenues[i] = 0;
+              let droneDeliveries = await getDroneDeliveries("drone", this.drones[i].id)
+              if (droneDeliveries.length >= 1){
+                  for(let j = 0; j < droneDeliveries.length; j++) {
+                      let transaction = await getTransaction(droneDeliveries[j].transaction_id);
+                      let revenueFromJob = transaction[0].sales_price / 2;
+                      this.revenues[i] += revenueFromJob;
+                  }
+                  
+              }
+              
+          }
+          console.log('Revenues: ', this.revenues);
+      } catch (error) {
+          console.log(error);
+      }
+    },
   },
-  async mounted() {
-    // You can call the getDrones function here or in any method as needed
-    try {
-      const limit = 50; // Specify your desired limit
-      this.drones = await getDrones(limit, this.id);
-      console.log('Drones:', this.drones);
-    } catch (error) {
-      console.error('Error fetching drones:', error);
-    }
-  }
 };
 </script>
