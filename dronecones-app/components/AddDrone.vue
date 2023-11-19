dcd17dd641fe9ad81777d9649f386bafae28cbfe
Jordan Olsen <a02325079@usu.edu>
2023-11-18 22:25:07 -0700

    Added label for size options when adding a drone

--- dronecones-app/components/AddDrone.vue
+++ dronecones-app/components/AddDrone.vue
@@ -32,7 +32,8 @@
                     <label for ="name">Name:</label>
                     <input type = "text" id = "name" v-model="formData.name"/>
                 </div>
-                <div class="radio-button-container">
+                <legend for="size">Size:</legend>
+                <fieldset class="radio-button-container" style="margin-top: 0px; border: none;" id = "size">
                     <input name = "size" class = "radio" type="radio" id="small" value = '0' v-model="formData.size">
                     <label class = "radio-button" for="small">Small</label>
 
@@ -41,7 +42,7 @@
 
                     <input name = "size" class = "radio" type="radio" id="large" value = '2' v-model="formData.size">
                     <label class = "radio-button" for="large">Large</label>
-                </div>
+                </fieldset>
                 <div class = "spacer">
                     <button type = "submit" style = "background-color: var(--accent-color); color: var(--font-accent);">Add</button>
                     <slot/>
