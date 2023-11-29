e312aa3a527be11130b3b807ad9957bafa1def5d
Samuel McMillan <a02261822@usu.edu>
2023-11-28 17:46:26 -0700

    Recent Order History has been beatified and it now can work with incomplete orders too

--- dronecones-app/components/RecentOrderHistory.vue
+++ dronecones-app/components/RecentOrderHistory.vue
@@ -1,71 +1,72 @@
 <script>
-    import { getUserOrderHistory } from '../database/transactionDatabase'
-    import { getCones, getIcecreamFlavors, getToppings } from '~/database/stockDatabase'
-    import * as transDb from '../database/transactionDatabase'
-    import { ref } from 'vue'
-    export default {
-        data() {
-            return {
-                pastOrders: [],
-                flavors: [],
-                cones: [],
-                toppings: [],
-                scoops: [],
-                userID: 0,
-            };
-        },
-        
-        async created() {
-            try {
-                this.userID = localStorage.getItem("userID");
-                const data = await getUserOrderHistory(localStorage.getItem('userID'), 3);
-                this.pastOrders = data;
-                
-                await getIcecreamFlavors().then(data => {
-                    for (let flavor of data) {
-                    this.flavors[flavor.id] = flavor.name;
-                    }
-                })
-                await getToppings().then(data => {
-                    for (let topping of data) {
-                    this.toppings[topping.id] = topping.name
-                    }
-                })
-                await getCones().then(data => {
-                    for (let cone of data) {
-                    this.cones[cone.id] = cone.name
-                    }
-                });
-            } catch (error) {
-                console.error('An error occurred:', error);
-            }
-        },
+import { getUserOrderHistory } from '../database/transactionDatabase'
+import { getCones, getIcecreamFlavors, getToppings } from '~/database/stockDatabase'
+import * as transDb from '../database/transactionDatabase'
+import { ref } from 'vue'
+export default {
+    data() {
+        return {
+            pastOrders: [],
+            flavors: [],
+            cones: [],
+            toppings: [],
+            scoops: [],
+            userID: 0,
+        };
+    },
+    
+    async created() {
+        try {
+            this.userID = localStorage.getItem("userID");
+            const data = await getUserOrderHistory(localStorage.getItem('userID'), 3);
+            this.pastOrders = data;
+            
+            await getIcecreamFlavors().then(data => {
+                for (let flavor of data) {
+                this.flavors[flavor.id] = flavor.name;
+                }
+            })
+            await getToppings().then(data => {
+                for (let topping of data) {
+                this.toppings[topping.id] = topping.name
+                }
+            })
+            await getCones().then(data => {
+                for (let cone of data) {
+                this.cones[cone.id] = cone.name
+                }
+            });
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    },
 
-    };
+    methods: {
+    async addItemInProgress(item) {
+        await transDb.addItemInProgress(item);
+    },
+},
 
+};
 </script>
 
 <template>
     <div>
         <h2 style="text-align: center">Ice Cream Order History</h2>
-        <ul style="text-align: center">
-            <li v-for="order in pastOrders" :key="order.id">
-                <h3 style="text-align: center">Cone: </h3>
-                {{ this.cones[order.orderItem[0].cone] }}
-                <h3 style="text-align: center">Flavors (per scoop): </h3>
-                {{ this.flavors[order.orderItem[0].flavor1] }}
-                {{ this.flavors[order.orderItem[0].flavor2] }}
-                {{ this.flavors[order.orderItem[0].flavor3] }}
-                <h3 style="text-align: center">Topping: </h3>
-                {{ this.toppings[order.orderItem[0].topping] }}
-                <p v-if="this.toppings[order.orderItem[0].topping] == null">
-                    None
-                </p>
-                <h3 style="text-align: center">Price: </h3>
-                {{ order.orderItem[0].price }}
-                <p></p>
-                <button class="order-button" :disabled="order.orderItem[0].flavor1 == null || order.orderItem[0].cone == null" @click="transDb.addItemInProgress({ cone:order.orderItem[0].cone, flavor1:order.orderItem[0].flavor1, flavor2:order.orderItem[0].flavor2, flavor3:order.orderItem[0].flavor3, price:order.orderItem[0].price, scoops:order.orderItem[0].scoops, topping:order.orderItem[0].topping, user_id:this.userID})">Add to Cart</button>
-            </li>
-        </ul>
+        <div style="text-align: center">
+            <div style="display: inline-block" v-for="order in pastOrders" :key="order.id">
+                <div class="order-history-item">
+                    <div class="history-container">
+                        <p v-if="order.orderItem[0].cone != null">{{ this.cones[order.orderItem[0].cone] }}</p>
+                        <p v-if="order.orderItem[0].flavor1 != null">Scoop 1: {{ this.flavors[order.orderItem[0].flavor1] }}</p>   
+                        <p v-if="order.orderItem[0].flavor2 != null">Scoop 2: {{ this.flavors[order.orderItem[0].flavor2] }}</p>
+                        <p v-if="order.orderItem[0].flavor3 != null">Scoop 3: {{ this.flavors[order.orderItem[0].flavor3] }}</p>
+                        <p v-if="order.orderItem[0].topping != null">Topping: {{ this.toppings[order.orderItem[0].topping] }}</p>
+                        <p v-if="order.orderItem[0].price != null">Price: {{ order.orderItem[0].price }}</p>
+                    </div>
+                    <button class="order-button" :disabled="order.orderItem[0].flavor1 == null || order.orderItem[0].cone == null" @click="addItemInProgress({ cone:order.orderItem[0].cone, flavor1:order.orderItem[0].flavor1, flavor2:order.orderItem[0].flavor2, flavor3:order.orderItem[0].flavor3, price:order.orderItem[0].price, scoops:order.orderItem[0].scoops, topping:order.orderItem[0].topping, user_id:this.userID});">Add to Cart</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
