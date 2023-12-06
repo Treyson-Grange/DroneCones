ee3bc86312acb528aefe3a90b579efcf02ced8e6
nathan <nathanctay@gmail.com>
2023-12-06 12:52:44 -0700

    Card fields are required now. Price displays in dollars instead of cents, and tax is rounded off at calculation

--- dronecones-app/components/Cart.vue
+++ dronecones-app/components/Cart.vue
@@ -67,7 +67,7 @@
     },
     methods: {
       getTax() {
-        this.tax = parseFloat((this.subtotal * .07).toFixed(2))
+        this.tax = Math.floor(this.subtotal * .07)
       },
       getTotal() {
         this.total = this.subtotal + this.tax
@@ -88,9 +88,9 @@
         console.log(this.id);
         console.log(ID);
         localStorage.setItem('orderID', ID);
-        for(let i = 0; i < this.orderItems.length; i++) {
-          await removeItemInProgress(this.orderItems[i].id)
-        }
+        // for(let i = 0; i < this.orderItems.length; i++) {
+        //   await removeItemInProgress(this.orderItems[i].id)
+        // }
         window.location = '/orderPlaced';
 
       },
@@ -131,32 +131,32 @@
           </div>
         </div>
       </div>
-      <h4 id="subtotal">Subtotal: {{ subtotal }}</h4>
-      <h4 id="tax">Tax: {{ tax }}</h4>
-      <h3 id="total">Your total is ${{ total }}</h3>
+      <h4 id="subtotal">Subtotal: {{ (subtotal / 100) }}</h4>
+      <h4 id="tax">Tax: {{ (tax / 100) }}</h4>
+      <h3 id="total">Your total is ${{ (total / 100) }}</h3>
       <form @submit.prevent="placeOrder()">
         <div class="form-group payment-input">
           <label for="card-name">Name on card</label>
-          <input type="text" id="card-name" v-model="formData.cardName"/>
+          <input type="text" id="card-name" v-model="formData.cardName" required/>
         </div>
         <div class="form-group payment-input">
           <label for="card-number">Card Number</label>
-          <input type="text" id="card-number" v-model="formData.cardNumber" />
+          <input type="text" id="card-number" v-model="formData.cardNumber" required/>
         </div>
         <div class="form-row centered payment-input">
           <div class="form-group">
             <label for="card-expiration">Card Expiration</label>
-            <input type="text" id="card-expiration" v-model="formData.cardExpiration"/>
+            <input type="text" id="card-expiration" v-model="formData.cardExpiration" required/>
           </div>
           <div class="form-group payment-input">
             <label for="security-code">Security code</label>
-            <input type="text" id="security-code" v-model="formData.securityCode"/>
+            <input type="text" id="security-code" v-model="formData.securityCode" required/>
           </div>
         </div>
         <div class="form-row centered payment-input">
           <div class="form-group address">
             <label for="address">Address Line 1</label>
-            <input type="text" id="address" v-model="formData.address"/>
+            <input type="text" id="address" v-model="formData.address" required/>
           </div>
           <div class="form-group apt payment-input">
             <label for="apt">Apt #</label>
@@ -165,15 +165,15 @@
         </div>
         <div class="form-group payment-input">
           <label for="city">City</label>
-          <input type="text" id="city" v-model="formData.city" />
+          <input type="text" id="city" v-model="formData.city" required/>
         </div>
         <div class="form-group payment-input">
           <label for="state">State</label>
-          <input type="text" id="state" v-model="formData.state"/>
+          <input type="text" id="state" v-model="formData.state" required/>
         </div>
         <div class="form-group payment-input">
           <label for="zip">Zip code</label>
-          <input type="text" id="zip" v-model="formData.zip"/>
+          <input type="text" id="zip" v-model="formData.zip" required/>
         </div>
         <div class="form-group centered payment-input">
           <button class="place-order-button" type="submit">Order</button>
